148680c006b56b647418700dab9795ec5c50fc14
EmiGashi <eminelg@gmail.com>
2021-11-30 19:35:13 +0000

    js file added

---
+++ skriptet.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = a;
+a = 35;
+
+document.write("Variable a eshte e barabarte me " + a);
+
+document.write("<br> Variable b eshte e barabarte me " + b);
+let shuma = a - b;
+
+let z = 26;
+document.write("<br><br>" + z);
+mbledhja();
+
+
+function mbledhja(){
+    let a = 200;
+    document.write("<br> <hr>Shumae tyre eshte e barabarte me " + shuma);
+    document.write("<br> <hr>Shumae tyre eshte e barabarte me " + a);
+}
+
+
+
